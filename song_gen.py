7c57bedf8b99c2e8598524edc8fae5a55b604fad
theairdemon <33183391+theairdemon@users.noreply.github.com>
2019-03-27 09:13:26 -0400

    Edits
    made some changes lol

--- song_gen.py
+++ song_gen.py
@@ -9,7 +9,7 @@ four_chords2 = ['VI', 'IV', 'I', 'V']
 
 songChords = []
 
-for i in epic_chords1:
+for i in four_chords1:
     songChords.append(i)
 
 percent_16 = 10
@@ -18,7 +18,7 @@ percent_4 = 50
 percent_2 = 0
 percent_1 = 0
 
-song1 = Generate(key_name, songChords[0], songChords[1],songChords[2], songChords[3], percent_16, percent_8, percent_4, percent_2, percent_1)
+song1 = Generate(key_name, songChords[0], songChords[1], songChords[2], songChords[3], percent_16, percent_8, percent_4, percent_2, percent_1)
 
 for i in range(0, 256):
     song1.one_measure(i+1)
