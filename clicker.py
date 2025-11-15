522830e6a75c9efb8709f02026e83d443d81a401
theairdemon <hgall7@gmail.com>
2025-11-14 19:23:58 -0500

    Minor edits idk

---
+++ clicker.py
@@ -0,0 +1,8 @@
+import pyautogui
+import time
+
+time.sleep(5)
+
+for i in range(0, 250):
+    pyautogui.click()
+
--- drinks.py
+++ drinks.py
@@ -4,57 +4,70 @@ from datetime import datetime
 
 def whatToDrink(base_liquor_options):
     base_liquor_dict = {
-        'tequila': [
-            'margarita',
-            'original johnny silverhand',
-            'tequila corpse reviver',
-            'tequila soda',
-            'french intervention',
-            'naked & famous'],
-        'gin': [
-            'cooperstown',
-            'martini (dry)',
-            'martini (perfect)',
-            'martini (dirty)',
-            'white negroni',
-            'negroni',
-            'the last word',
-            'corpse reviver #2',
-            'bees knees',
-            'gin rickey',
-            'alaska',
-            'french pearl',
-            'perfect white negroni (from the Wells in DC)',
-            'bijou'],
-        'whiskey': [
-            'boulevardier',
-            'manhattan',
-            'old fashioned',
-            'whiskey sour',
-            'gold rush',
-            'whiskey highball',
-            'whiskey & coke',
-            'pickleback shot',
-            'Snoopy'],
-        'absinthe': [
-            'straight absinthe with sugar',
-            'necromancer'],
-        'rum': [
-            'mai tai',
-            'rum old fashioned'
+        "tequila": [
+            "margarita",
+            "original johnny silverhand",
+            "tequila corpse reviver",
+            "tequila soda",
+            "french intervention",
+            "naked & famous",
         ],
-        'non-liquor': [
-            'blue moon',
-            'guinness',
-            'cranberry long drink',
-            'peach long drink',
+        "gin": [
+            "cooperstown",
+            "martini (dry)",
+            "martini (perfect)",
+            "martini (dirty)",
+            "white negroni",
+            "negroni",
+            "the last word",
+            "corpse reviver #2",
+            "bees knees",
+            "gin rickey",
+            "alaska",
+            "french pearl",
+            "perfect white negroni (from the Wells in DC)",
+            "bijou",
+            "Giny Germain",
+            "Julie's pasta martini",
         ],
-        'other': [
-            'chirulin',
-            'pisco sour',
-            'espresso martini',
-            'sidecar',
-            'protein cocktail abomination']
+        "whiskey": [
+            "boulevardier",
+            "manhattan",
+            "old fashioned",
+            "whiskey sour",
+            "gold rush",
+            "whiskey highball",
+            "whiskey & coke",
+            "pickleback shot",
+            "Snoopy",
+        ],
+        "absinthe": [
+            "straight absinthe with sugar",
+            "necromancer",
+            "Death in the Mourning",
+        ],
+        "rum": [
+            "mai tai",
+            "rum old fashioned",
+            "daiquiri",
+            "mojito",
+            "Hemingway Daiquiri",
+        ],
+        "non-liquor": [
+            "blue moon",
+            "guinness",
+            "cranberry long drink",
+            "peach long drink",
+            "sam adams",
+        ],
+        "other": [
+            "chirulin",
+            "pisco sour",
+            "espresso martini",
+            "sidecar",
+            "protein cocktail abomination",
+            "Apple Pie Sour",
+        ],
     }
 
     # random.seed(datetime.now().timestamp())
@@ -67,17 +80,22 @@ def whatToDrink(base_liquor_options):
     chosen_liquor = random.choice(base_liquor_options)
 
     chosen_liquor_string = "Base: " + chosen_liquor
-    cocktail_string = "Cocktail: " + \
-        random.choice(base_liquor_dict[chosen_liquor])
-    return "==================\n" + chosen_liquor_string + "\n" + cocktail_string + "\n=================="
+    cocktail_string = "Cocktail: " + random.choice(base_liquor_dict[chosen_liquor])
+    return (
+        "==================\n"
+        + chosen_liquor_string
+        + "\n"
+        + cocktail_string
+        + "\n=================="
+    )
 
 
 liquor_list = [
-    'gin',
-    'whiskey',
-    # 'tequila',
-    'absinthe',
-    'rum'
+    "gin",
+    # 'whiskey',
+    "tequila",
+    "absinthe",
+    # 'rum',
     # 'non-liquor',
     # 'other'
 ]
